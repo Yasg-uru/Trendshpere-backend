3b908ab6fd09c7707248bdbad52dbd1f8f20f3be
yasg-uru <yashpawar12122004@gmail.com>
2025-01-01 14:28:22 +0530

    corrected configuration of the cors for the frontend deployment

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://trendsphere-three.vercel.app"],
+    origin: ["http://localhost:5173", "https://trendsphere-frontend.vercel.app"],
 
     credentials: true,
   })
 );
 const io = new SocketServer(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://trendsphere-three.vercel.app"],
+    origin: ["http://localhost:5173", "https://trendsphere-frontend.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
